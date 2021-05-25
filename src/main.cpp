d1a3428aa55c4ba727e4b98f796d660f2347b7dc
Federico Roux <rouxfederico@gmail.com>
2021-05-25 19:56:50 -0300

    code merge with saadc project working

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,9 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
 
 #ifdef __cplusplus
 extern "C" {
@@ -64,6 +67,23 @@ extern "C" {
 #include "sdk_errors.h"
 #include "app_error.h"
 
+
+#include "nrf.h"
+#include "nrf_drv_saadc.h"
+#include "nrf_drv_ppi.h"
+#include "nrf_drv_timer.h"
+#include "boards.h"
+#include "app_error.h"
+#include "nrf_delay.h"
+#include "app_util_platform.h"
+#include "nrf_pwr_mgmt.h"
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
+
+
 #ifdef __cplusplus
 }
 #endif 
@@ -74,6 +94,150 @@ extern "C" {
 #if LEDS_NUMBER <= 2
 #error "Board is not equipped with enough amount of LEDs"
 #endif
+
+
+// --------------------
+#define SAMPLES_IN_BUFFER 5
+volatile uint8_t state = 1;
+
+static const nrf_drv_timer_t m_timer = NRF_DRV_TIMER_INSTANCE(0);
+static nrf_saadc_value_t     m_buffer_pool[2][SAMPLES_IN_BUFFER];
+static nrf_ppi_channel_t     m_ppi_channel;
+static uint32_t              m_adc_evt_counter;
+
+
+void timer_handler(nrf_timer_event_t event_type, void * p_context)
+{
+
+}
+
+
+void saadc_sampling_event_init(void)
+{
+    ret_code_t err_code;
+
+    err_code = nrf_drv_ppi_init();
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_timer_config_t timer_cfg = NRF_DRV_TIMER_DEFAULT_CONFIG;
+    timer_cfg.bit_width = NRF_TIMER_BIT_WIDTH_32;
+    err_code = nrf_drv_timer_init(&m_timer, &timer_cfg, timer_handler);
+    APP_ERROR_CHECK(err_code);
+
+    /* setup m_timer for compare event every 400ms */
+    uint32_t ticks = nrf_drv_timer_ms_to_ticks(&m_timer, 400);
+    nrf_drv_timer_extended_compare(&m_timer,
+                                   NRF_TIMER_CC_CHANNEL0,
+                                   ticks,
+                                   NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK,
+                                   false);
+    nrf_drv_timer_enable(&m_timer);
+
+    uint32_t timer_compare_event_addr = nrf_drv_timer_compare_event_address_get(&m_timer,
+                                                                                NRF_TIMER_CC_CHANNEL0);
+    uint32_t saadc_sample_task_addr   = nrf_drv_saadc_sample_task_get();
+
+    /* setup ppi channel so that timer compare event is triggering sample task in SAADC */
+    err_code = nrf_drv_ppi_channel_alloc(&m_ppi_channel);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = nrf_drv_ppi_channel_assign(m_ppi_channel,
+                                          timer_compare_event_addr,
+                                          saadc_sample_task_addr);
+    APP_ERROR_CHECK(err_code);
+}
+
+
+void saadc_sampling_event_enable(void)
+{
+    ret_code_t err_code = nrf_drv_ppi_channel_enable(m_ppi_channel);
+
+    APP_ERROR_CHECK(err_code);
+}
+
+
+void saadc_callback(nrf_drv_saadc_evt_t const * p_event)
+{
+    if (p_event->type == NRF_DRV_SAADC_EVT_DONE)
+    {
+        ret_code_t err_code;
+
+        err_code = nrf_drv_saadc_buffer_convert(p_event->data.done.p_buffer, SAMPLES_IN_BUFFER);
+        APP_ERROR_CHECK(err_code);
+
+        int i;
+        NRF_LOG_INFO("ADC event number: %d", (int)m_adc_evt_counter);
+
+        for (i = 0; i < SAMPLES_IN_BUFFER; i++)
+        {
+            NRF_LOG_INFO("%d", p_event->data.done.p_buffer[i]);
+        }
+        m_adc_evt_counter++;
+    }
+}
+
+
+void saadc_init(void)
+{
+    ret_code_t err_code;
+    nrf_saadc_channel_config_t channel_config =
+        NRF_DRV_SAADC_DEFAULT_CHANNEL_CONFIG_SE(NRF_SAADC_INPUT_AIN0);
+
+    err_code = nrf_drv_saadc_init(NULL, saadc_callback);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = nrf_drv_saadc_channel_init(0, &channel_config);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = nrf_drv_saadc_buffer_convert(m_buffer_pool[0], SAMPLES_IN_BUFFER);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = nrf_drv_saadc_buffer_convert(m_buffer_pool[1], SAMPLES_IN_BUFFER);
+    APP_ERROR_CHECK(err_code);
+
+}
+
+
+int ADC_init(void) {
+
+    uint32_t err_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+
+    ret_code_t ret_code = nrf_pwr_mgmt_init();
+    APP_ERROR_CHECK(ret_code);
+
+    saadc_init();
+    saadc_sampling_event_init();
+    saadc_sampling_event_enable();
+    NRF_LOG_INFO("SAADC HAL simple example started.");
+
+    return 0;
+}
+
+    /**
+     * 
+     * @brief ADC pwr mgmt run
+     *
+     */
+
+    void nrf_log_flush_task (void * pvParameter)
+    {
+        UNUSED_PARAMETER(pvParameter);
+        while (true)
+        {
+            nrf_pwr_mgmt_run();
+            NRF_LOG_FLUSH();
+        }
+    }
+
+
+// -------------------------------
+
+
+
+
 
 int main(void)
 {
@@ -87,9 +251,18 @@ int main(void)
     /* Configure LED-pins as outputs */
     bsp_board_init(BSP_INIT_LEDS);
 
+    ADC_init();
+
+
     /* Create task for LED0 blinking with priority set to 2 */
     UNUSED_VARIABLE(xTaskCreate(led::toggle_task_function, "LED0", 
         configMINIMAL_STACK_SIZE + 200, NULL, 2, &led::toggle_task_handle));
+
+
+    /* Create task for NRF Log flush */
+    UNUSED_VARIABLE(xTaskCreate(nrf_log_flush_task, "nrf-log", 
+        configMINIMAL_STACK_SIZE + 200, NULL, 2, NULL));
+
 
     /* Start timer for LED1 blinking */
     led::toggle_timer_handle = xTimerCreate( "LED1", led::timer_period, pdTRUE, NULL, 
